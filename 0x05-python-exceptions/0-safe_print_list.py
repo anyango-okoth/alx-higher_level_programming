ae8c909ebeea1fe22eb46fb1afecbfdce41d7746
anyango-okoth <anyangomoureen02@gmail.com.com>
2023-07-31 13:01:04 -0700

    safe_print_list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,18 +1,14 @@
-#!/usr/bin/python3
-def count_elements(my_list):
-    count = 0
-    for _ in my_list:
-        count += 1
-    return count
-
 def safe_print_list(my_list=[], x=0):
-    try:
-        count = count_elements(my_list)
-        num_to_print = min(count, x)
-        for i in range(num_to_print):
-            print(my_list[i], end=" ")
-    except TypeError:
-        pass
-    finally:
-        print()  # Print a new line after printing the elements
-    return num_to_print
+    """Print x elements of a list.
+
+    Args:
+        my_list (list): The list to print elements from.
+        x (int): The number of elements of my_list to print.
+
+    Returns:
+        The number of elements printed.
+    """
+    elements_to_print = my_list[:x]
+    print("".join(map(str, elements_to_print)))
+    return len(elements_to_print)
+
