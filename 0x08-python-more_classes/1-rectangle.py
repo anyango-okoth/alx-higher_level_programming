c5ebba357dc49de387e616ee3445df52e633dc45
anyango-okoth <anyangomoureen02@gmail.com>
2023-08-08 02:20:17 +0000

    Private instance attribute

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-
 class Rectangle:
     def __init__(self, width=0, height=0):
-        self.width = width
-        self.height = height
+        # Initialize the private attributes for width and height
+        self.__width = width
+        self.__height = height
 
     @property
     def width(self):
+        # Getter for width attribute
         return self.__width
 
     @width.setter
     def width(self, value):
+        # Setter for width attribute
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -19,15 +21,15 @@ class Rectangle:
 
     @property
     def height(self):
+        # Getter for height attribute
         return self.__height
 
     @height.setter
     def height(self, value):
+        # Setter for height attribute
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-
-  
 
