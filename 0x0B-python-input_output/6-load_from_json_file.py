4df421e89a03bc33e71bc4857df62aaeadba4311
anyango-okoth <anyangomoureen02@gmail.com>
2023-10-10 11:15:45 -0700

    load_from_json_file.

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Define json-to-object function"""
+import json
+
+
+def load_from_json_file(filename):
+    """function that creates an Object from a “JSON file”"""
+    with open(filename) as f:
+        return json.load(f)
