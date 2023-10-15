7bf8e702806a53b93a416ca1679f557d23550d6e
anyango-okoth <anyangomoureen02@gmail.com>
2023-10-15 05:57:57 -0700

    sorted-list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,8 @@
+!/usr/bin/python3
+"""Defines an inherited class Mylist."""
+
+class MyList(list):
+    """prints the list, but sorted (ascending sort)."""
+
+    def print_sorted(self):
+        print(sorted(self))
