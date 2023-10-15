477746ab20ef82e90f76151b6f6da162d3f9837c
anyango-okoth <anyangomoureen02@gmail.com>
2023-10-15 11:31:03 -0700

    print-square

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,19 @@
+
+""" Define printing of squares """
+def print_square(size):
+    """ print a size of a square with # character
+
+    Args:
+    size(int): height/width of the square
+    Raises:
+    TypeError: size is not an integer
+    ValueError: size must be < 0
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        [print("#", end="")for i in range(size)]
+        print("")
